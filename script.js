e5b5b9739da5190ce5373c308f794393da14378e
LakshmiNarayanan280406 <planar2006@gmail.com>
2024-04-18 22:10:54 +0530

    Added functionality to the rest of the buttons.

--- script.js
+++ script.js
@@ -2,6 +2,79 @@ const display=document.querySelector('.display')
 const allDigits=document.querySelectorAll('.digit')
 const allClear=document.querySelector('.AC')
 const delchar=document.querySelector('.delchar')
+const dot=document.querySelector('.dot')
+const operators=document.querySelectorAll('.operator')
+const equal=document.querySelector('.equalto')
+let first_operand=''
+let second_operand=''
+let operation=''
+let result;
+
+function add(a,b){
+    result= parseFloat(a)+parseFloat(b);
+    first_operand=result
+    second_operand=''
+    operation=''
+    return Math.round(result * 1000)/1000
+}
+
+function sub(a,b){
+    
+    result = parseFloat(a)-parseFloat(b);
+    first_operand=result
+    second_operand=''
+    operation=''
+    return Math.round(result * 1000)/1000
+}
+
+function multiply(a,b){
+    result = (parseFloat(a))*(parseFloat(b));
+    first_operand=result
+    second_operand=''
+    operation=''
+    return Math.round(result * 1000)/1000
+}
+
+function div(a,b){
+    if (b==='0'){
+        result ='nah';
+        first_operand=''
+        second_operand=''
+        operation=''
+        return result
+    }
+    else{
+        result = parseFloat(a)/parseFloat(b);
+        first_operand=result
+        second_operand=''
+        operation=''
+        return Math.round(result * 1000)/1000
+    }
+}
+
+// Operate
+function operate(){
+    if (first_operand && second_operand && operation){
+        switch(operation){
+            case '+':
+                display.textContent=add(first_operand,second_operand);
+                break;
+            case '-':
+                display.textContent=sub(first_operand,second_operand);
+                break;
+            case 'x':
+                display.textContent=multiply(first_operand,second_operand)
+                break;
+            case '/':
+                display.textContent=div(first_operand,second_operand);
+                break
+        }
+    }
+    else{
+        return;
+    }
+}
+
 
 // Function for inputting numbers
 
@@ -13,14 +86,26 @@ function inputDigit(){
         return;
     }
     else{ 
-        display.textContent+=this.textContent;
+        if (operation === ''){
+            first_operand+=this.textContent
+            display.textContent=first_operand
+        }
+        else{
+            second_operand+=this.textContent
+            display.textContent=second_operand
+        }
+        
     }
 }
 
 // Function to  clear the screen
 
 function clear(){
-    display.textContent='0'
+    display.textContent='0';
+    first_operand=''
+    second_operand=''
+    operation=''
+    result=''
 }
 // Function to delete the previous character
 
@@ -30,6 +115,19 @@ function delCharFn(){
     }
     else{
         display.textContent='0';
+    }
+}
+
+// Function for period
+
+function dotFn(){
+    if (operation === ''){
+        first_operand+=this.textContent
+        display.textContent=first_operand
+    }
+    else{
+        second_operand+=this.textContent
+        display.textContent=second_operand
     }
 }
 
@@ -44,3 +142,22 @@ for (let digit of allDigits){
     digit.addEventListener('click',inputDigit);
 }
 
+dot.addEventListener('click',dotFn)
+
+for (let operator of operators){
+    operator.addEventListener('click',function (){
+        if (first_operand === ''){
+            return;
+        }
+        else if (second_operand === ''){ 
+            display.textContent=first_operand;
+            operation=this.textContent;
+        }
+        else{
+            operate();
+            operation=this.textContent;
+        }
+    })
+}
+
+equal.addEventListener('click',operate)
